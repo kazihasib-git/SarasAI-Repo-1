23777fed913a2ec48fe25e182e8c08722b768fbf
Divyansh Bansal <divyanshbansal@dynapt.co.in>
2024-07-03 15:08:34 +0530

    create new slot

--- SARAS_AI_FRONTEND/src/components/CustomFields/CustomDateField.jsx
+++ SARAS_AI_FRONTEND/src/components/CustomFields/CustomDateField.jsx
@@ -1,30 +1,32 @@
 import React from "react";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider, DatePicker } from "@mui/x-date-pickers";
-import CustomTextField from "./CustomTextField"; // Adjust the path according to your file structure
-import dayjs from "dayjs";
+import moment from "moment";
+
 const CustomDateField = ({
   label,
   name,
-  register,
-  validation,
-  errors,
-  value, // Ensure `value` is passed and used properly
+  value,
   onChange,
   sx,
   ...props
 }) => {
-  const dayjsValue = typeof value === "string" ? dayjs(value) : value;
+  const momentValue = value ? moment(value, "YYYY-MM-DD") : null;
+
+  const handleDateChange = (date) => {
+    onChange(date.format("YYYY-MM-DD"));
+  };
+
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
-      {console.log}
       <DatePicker
         label={label}
-        name={name} // Ensure `name` is passed if needed
-        value={dayjsValue} // Pass the date value received from props
-        onChange={onChange} // Handle onChange event to update the date value
+        name={name}
+        value={momentValue}
+        onChange={handleDateChange}
+        inputFormat="YYYY-MM-DD"
         InputLabelProps={{
-          shrink: true, // Ensure the label behaves correctly
+          shrink: true,
         }}
         {...props}
         sx={{
@@ -44,7 +46,7 @@ const CustomDateField = ({
             margin: 0,
             color: '#1A1E3D',
             '&.Mui-focused': {
-                color: '#1A1E3D', // Change label color on focus
+                color: '#1A1E3D',
             },
           },
           ...sx,
